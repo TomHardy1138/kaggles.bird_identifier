7db4f744682363e22b65feaf6c8bd957b9c7b1d5
Michael <m.vaschanov@yandex.ru>
2021-05-04 08:59:42 +0300

    conv layer in model.py

--- model.py
+++ model.py
@@ -8,14 +8,15 @@ class Network(nn.Module):
         self.model = timm.create_model(
             backbone,
             num_classes=0,
-            pretrained=False,
+            pretrained=True,
             **kwargs
         )
+        self.start_conv = nn.Conv2d(1, 3, (3, 3))
         self.classifier = nn.Linear(self.model.num_features, num_classes)
         self.metric_losses = metric_losses
 
     def forward(self, x):
-        features = self.model(x)
+        features = self.model(self.start_conv(x))
         preds = self.classifier(features)
 
         if self.training and self.metric_losses:
